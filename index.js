a41d613ccc6b7819517d9c0fc3236972be5a3691
moazzamkhan112 <164583812+moazzamkhan112@users.noreply.github.com>
2024-04-08 20:41:14 +0500

    push my word counter

---
+++ index.js
@@ -0,0 +1,13 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter your sentence to count the words: "
+    }
+]);
+const answerinput = answer.sentence;
+//for counting letters  in sentence 
+const wordswithoutspace = answerinput.replace(/\s/g, "");
+const wordscount = wordswithoutspace.length;
+console.log(`your sentence to count the words is ${wordscount}`);
